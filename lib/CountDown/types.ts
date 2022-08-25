1cee4771e334cdbefd7fda1ba99adce851656d37
2nthony <ant.hu@outlook.com>
2022-08-25 15:33:21 +0800

    revert: no need to render estimate string

--- lib/CountDown/types.ts
+++ lib/CountDown/types.ts
@@ -1,20 +1,10 @@
-import React, { ReactNode } from "react";
-import { extractTime } from "@polkadot/util";
-
-export type EstimateTime = ReturnType<typeof extractTime>;
-
-export type Time = EstimateTime & { defaultTimeString: string };
+import React from "react";
 
 export type CountDownProps = React.SVGAttributes<SVGSVGElement> & {
-  /**
-   * @default 12000
-   */
-  blockTime?: number;
   blockHeight?: number;
   startBlockHeight?: number;
   endBlockHeight?: number;
   size?: number;
   circleForegroundColor?: string;
   circleBackgroundColor?: string;
-  timeRender?(time: Time): ReactNode;
 };
