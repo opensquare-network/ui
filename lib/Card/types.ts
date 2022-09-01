f995e9369e6efaa5027ad91a74cf7d0646e1d64b
2nthony <ant.hu@outlook.com>
2022-09-01 10:29:35 +0800

    fix(card): props types is optional

--- lib/Card/types.ts
+++ lib/Card/types.ts
@@ -1,13 +1,13 @@
 import { ReactNode } from "react";
 
 export type CardProps = React.HTMLAttributes<HTMLDivElement> & {
-  children: ReactNode;
-  title: ReactNode;
-  extra: ReactNode;
-  head: ReactNode;
-  size: "small" | undefined;
-  shadow: boolean;
-  bordered: boolean;
-  prefix: ReactNode;
-  suffix: ReactNode;
+  children?: ReactNode;
+  title?: ReactNode;
+  extra?: ReactNode;
+  head?: ReactNode;
+  size?: "small" | undefined;
+  shadow?: boolean;
+  bordered?: boolean;
+  prefix?: ReactNode;
+  suffix?: ReactNode;
 };
