a1ee63818c074a8b2767371558ceb6e74701462e
2nthony <ant.hu@outlook.com>
2023-05-15 12:00:33 +0800

    chore(deps): deps and fix storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,46 +1,41 @@
 module.exports = {
-  core: {
-    builder: "webpack5",
-  },
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
-  framework: "@storybook/react",
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
   // https://github.com/storybookjs/storybook/issues/6188#issuecomment-822924831
-  webpackFinal: (config) => {
-    const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test.test(".svg"),
-    );
+  webpackFinal: config => {
+    const fileLoaderRule = config.module.rules.find(rule => rule.test.test(".svg"));
     fileLoaderRule.exclude = /\.svg$/;
 
     // https://github.com/facebook/create-react-app/blob/f99167c014a728ec856bda14f87181d90b050813/packages/react-scripts/config/webpack.config.js#L388
     config.module.rules.push({
       test: /\.svg$/,
-      use: [
-        {
-          loader: "@svgr/webpack",
-          options: {
-            prettier: false,
-            svgo: false,
-            svgoConfig: {
-              plugins: [{ removeViewBox: false }],
-            },
-            titleProp: true,
-            ref: true,
+      use: [{
+        loader: "@svgr/webpack",
+        options: {
+          prettier: false,
+          svgo: false,
+          svgoConfig: {
+            plugins: [{
+              removeViewBox: false
+            }]
           },
-        },
-        {
-          loader: "file-loader",
-          options: {
-            name: "static/media/[name].[hash].[ext]",
-          },
-        },
-      ],
+          titleProp: true,
+          ref: true
+        }
+      }, {
+        loader: "file-loader",
+        options: {
+          name: "static/media/[name].[hash].[ext]"
+        }
+      }]
     });
-
     return config;
   },
+  docs: {
+    autodocs: true
+  }
 };
