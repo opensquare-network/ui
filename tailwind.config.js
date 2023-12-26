74a83bbb59a2838afdc485ef3c8d6b15d478c82c
2nthony <ant.hu@outlook.com>
2023-12-26 18:34:42 +0800

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 const { colorTokens } = require("./lib/styles/tailwind/tokens");
+const path = require("node:path");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ["./lib/**/*.{js,jsx}"],
+  content: [path.resolve(__dirname, "lib/**/*.{js,jsx}")],
   theme: {
     extend: {
       colors: {
