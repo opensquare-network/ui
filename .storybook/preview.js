31cfe46f44931edd80aba2ae132c30340c763c1c
2nthony <ant.hu@outlook.com>
2023-12-26 18:34:29 +0800

    update storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,7 @@
 import "semantic-ui-css/semantic.min.css";
+import "../lib/styles/index.css";
 import "../lib/styles/reset.css";
+import { withThemeByClassName } from "@storybook/addon-themes";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -10,3 +12,13 @@ export const parameters = {
     },
   },
 };
+
+export const decorators = [
+  withThemeByClassName({
+    themes: {
+      light: "light",
+      dark: "dark",
+    },
+    defaultTheme: "light",
+  }),
+];
