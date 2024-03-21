3bbb072ad7cfde32c7f69f72626bf56e7e7b57bb
2nthony <ant.hu@outlook.com>
2024-03-21 17:23:52 +0800

    chore: fix dts error from eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,16 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
     node: true,
     es2021: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended", "@osn"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "@osn",
+  ],
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
