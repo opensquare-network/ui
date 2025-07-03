3e7748c67016c51f646b69226d873d93e9e18907
wolyshaw <wolyshaw@users.noreply.github.com>
2025-07-03 16:30:11 +0800

    fix: time error

--- src/stories/Time.stories.js
+++ src/stories/Time.stories.js
@@ -7,3 +7,4 @@ export default {
 
 export const primary = () => <Time time="2022-04-13" />;
 export const yearsAgo = () => <Time time="2012-12-21" />;
+export const fromNow = () => <Time time="2125-12-31" />;
